7049c20d6095990515814d3eff1ed18d26c1a6e0
mriusero <160402415+mriusero@users.noreply.github.com>
2025-04-09 08:43:56 +0200

    feat: add existing tools

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import requests
 import pytz
 import yaml
 from tools.final_answer import FinalAnswerTool
+from tools.web_search import DuckDuckGoSearchTool
+from tools.visit_webpage import VisitWebpageTool
 
 from Gradio_UI import GradioUI
 
@@ -36,6 +38,8 @@ def get_current_time_in_timezone(timezone: str) -> str:
 
 
 final_answer = FinalAnswerTool()
+web_search = DuckDuckGoSearchTool()
+visit_webpage = VisitWebpageTool()
 
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
 # model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
@@ -57,7 +61,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[final_answer, web_search, visit_webpage, get_current_time_in_timezone], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
